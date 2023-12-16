48f7606ada92ebc7385f4158e3e448d6ab7633c4
kurokurotho <deoruchi064@gmail.com>
2023-12-17 00:11:57 +0530

    adding the page

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to the space</h1>");
+});
+
+app.get("/page1", (req, res) => {
+  res.sendFile(__dirname + "/files/page1.html");
 });
 
 app.listen(process.env.PORT, () => {
